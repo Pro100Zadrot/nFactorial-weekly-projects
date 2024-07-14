b47f5bb0b29de1e8a5452f6751f670969e4670c6
Pro100Zadrot <124894042+Pro100Zadrot@users.noreply.github.com>
2024-07-15 04:49:23 +0500

    Add files via upload

---
+++ Final-week/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>First steps</h1>
+      <nav>
+        <ul>
+          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+          <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+          <li><Link to="contacts" smooth={true} duration={500}>Contacts</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
